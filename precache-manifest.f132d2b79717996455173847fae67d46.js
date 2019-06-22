7e977f9f9fee7d11e4e3dbf8d833a48e6fc790e5
Kuat Kobekpayev <kuat@ape.kz>
2019-06-22 16:08:34 +0600

    Updates

---
+++ precache-manifest.f132d2b79717996455173847fae67d46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c285418b0505e4b77c265e70cba4f196",
+    "url": "/test-assignment-black-scholes/index.html"
+  },
+  {
+    "revision": "5c8f715f951c96195aa5",
+    "url": "/test-assignment-black-scholes/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5c8f715f951c96195aa5",
+    "url": "/test-assignment-black-scholes/static/js/2.018b3161.chunk.js"
+  },
+  {
+    "revision": "d38838d57d7515db0a7a",
+    "url": "/test-assignment-black-scholes/static/js/main.905ff85f.chunk.js"
+  },
+  {
+    "revision": "498ad2f89f574e09a18c",
+    "url": "/test-assignment-black-scholes/static/js/runtime~main.57d543ed.js"
+  }
+]);
