5143f5afe1eb7563d0a3c5a49d90eeff109cdff0
Kuat Kobekpayev <kuat@ape.kz>
2019-06-22 11:15:05 +0600

    App grid: column component

---
+++ src/components/Column.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const Column = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 420px;
+  padding: 0 30px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export default Column;
