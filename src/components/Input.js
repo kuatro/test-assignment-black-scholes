41f8234c03957242afda10be7e3b69bed10a721f
Kuat Kobekpayev <kuat@ape.kz>
2019-06-22 11:16:28 +0600

    Form input

---
+++ src/components/Input.js
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+const Input = styled.input`
+  box-sizing: border-box;
+  width: 100%;
+  height: 60px;
+  padding: 0 30px;
+  margin: 6px 0 24px 0;
+  border: none;
+  box-shadow: ${props => props.theme.shadows.inputDefault};
+  appearance: none;
+  font-family: ${props => props.theme.fonts.primary};
+  font-size: 20px;
+  color: ${props => props.theme.colors.primary};
+
+  &:focus {
+    outline: none;
+  }
+
+  &:placeholder {
+    color: ${props => props.theme.colors.secondary};
+  }
+`;
+
+export default Input;
--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,12 @@ const theme = {
     primary: '"Alegreya Sans", sans-serif'
   },
   colors: {
-    primary: '#000000'
+    primary: '#000000',
+    secondary: '#BFBAB6'
+  },
+  shadows: {
+    inputDefault: '0px 0px 12px #F2F0ED, 0px 1px 3px #E6E3E1',
+    inputError: '0px 3px 12px rgba(255, 0, 30, 0.25), 0px 1px 3px rgba(255, 0, 30, 0.25)'
   }
 }
 
